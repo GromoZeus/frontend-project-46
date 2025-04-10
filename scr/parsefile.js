5952956984b92e7d5cb38de00fd3cf2fe9fe67c5
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-04-10 22:59:14 +0700

    mod parsefile.js

--- scr/parsefile.js
+++ scr/parsefile.js
@@ -3,14 +3,9 @@ import { resolve, isAbsolute } from 'path';
 import { cwd } from 'process';
 
 export default (file) => {
-  let fullPath = '';
-  if (isAbsolute(file)) {
-    fullPath = file;
-  } else {
-    fullPath = resolve(cwd(), file);
-  }
   try {
-    return JSON.parse(readFileSync(fullPath, 'utf8'));
+    return isAbsolute(file) ? JSON.parse(readFileSync(file, 'utf8'))
+      : JSON.parse(readFileSync(resolve(cwd(), file), 'utf8'));
   } catch (error) {
     console.error(`Error reading file: ${error.message}`);
     return null;
