43c3231ea028bb8b835fdac57e07487981cdfd2c
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-04-16 23:11:08 +0700

    modified __tests__

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { findDiff, formatDiff } from '../src/index.js';
+import { findDiff } from '../src/index.js';
 
 describe('findDiff', () => {
   test('should find differences between two flat objects', () => {
@@ -42,53 +42,5 @@ describe('findDiff', () => {
       { key: 'a', type: 'unchanged', value: 1 },
       { key: 'b', type: 'unchanged', value: 2 },
     ]);
-  });
-});
-
-describe('formatDiff', () => {
-  test('should format differences in stylish format', () => {
-    const diff = [
-      { key: 'a', type: 'removed', value: 1 },
-      { key: 'b', type: 'unchanged', value: 2 },
-      {
-        key: 'c', type: 'changed', oldValue: 3, newValue: 4,
-      },
-      { key: 'd', type: 'added', value: 5 },
-    ];
-    const expected = `{
-  - a: 1
-    b: 2
-  - c: 3
-  + c: 4
-  + d: 5
-}`;
-    expect(formatDiff(diff)).toBe(expected);
-  });
-
-  test('should format nested differences in stylish format', () => {
-    const diff = [
-      {
-        key: 'a',
-        type: 'nested',
-        children: [
-          { key: 'x', type: 'unchanged', value: 1 },
-          { key: 'y', type: 'removed', value: 2 },
-          { key: 'z', type: 'added', value: 3 },
-        ],
-      },
-      {
-        key: 'b', type: 'changed', oldValue: 2, newValue: 3,
-      },
-    ];
-    const expected = `{
-    a: {
-        x: 1
-      - y: 2
-      + z: 3
-    }
-  - b: 2
-  + b: 3
-}`;
-    expect(formatDiff(diff)).toBe(expected);
   });
 });
